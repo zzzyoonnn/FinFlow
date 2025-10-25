8c48c58b9a327ff8da7de33022d7b7f22a835f97
zzzyoonnn <dwsrh03@gmail.com>
2025-10-25 11:46:11 +0900

    chore: initialize FinFlow project with Spring Boot setup

---
+++ src/test/java/com/example/FinFlow/FinFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
